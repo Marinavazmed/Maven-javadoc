4e763fbe9ff2b43f05866fe6f51d6057c66c4243
marinavazmed <marina.vazquez.medinilla.al@iespoligonosur.org>
2023-03-21 13:17:48 +0100

    initial

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Main {
+
+	/**
+	 * Clase de prueba JavaDoc con Maven
+	 * @author marinavazquez
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
